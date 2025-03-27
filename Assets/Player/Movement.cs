46986ddb0ce27432a067a6716d660130a6518409
Marek Setikovský <126716456+Chlomek@users.noreply.github.com>
2025-03-27 17:07:59 +0100

    Added world border

--- Assets/Player/Movement.cs
+++ Assets/Player/Movement.cs
@@ -7,6 +7,8 @@ public class Movement : MonoBehaviour
     public float moveSpeed = 1;
     public Vector2 lookDirection = Vector2.right;
 
+    public float worldWidth = 500f;
+    public float worldHeight = 500f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,8 +21,15 @@ public class Movement : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+
         Vector2 direction = new Vector2(horizontalInput, verticalInput);
         transform.Translate(direction.normalized * moveSpeed * Time.deltaTime);
+
+        Vector2 playerPosition = transform.position;
+        float clampedX = Mathf.Clamp(playerPosition.x, -worldWidth / 2f, worldWidth / 2f);
+        float clampedY = Mathf.Clamp(playerPosition.y, -worldHeight / 2f, worldHeight / 2f);
+
+        transform.position = new Vector2(clampedX, clampedY);
 
         if (horizontalInput != 0 || verticalInput != 0)
         {
